9230bb2fe8853c6a48bb26988f3b8447ceae6155
David Tarvin <david.tarvin@physiciansmutual.com>
2018-11-27 08:57:45 -0600

    first commit for Primitive Types assignment

---
+++ week-1/Tarvin-Primitive-Types.js
@@ -0,0 +1,29 @@
+// start program
+
+/*
+    Expected output:
+    
+    First name
+    Last name
+    Age
+    Street
+    City
+    State 
+    Zip code 
+*/ 
+
+// variable declaration and assignment goes here ...
+var firstName = "David";
+var lastName = "Tarvin";
+var age = 49;
+var street = "14024 Manderson Plz Apt 305";
+var city = "Omaha";
+var state = "NE";
+var zipCode = "68164";
+
+// output 
+console.log(firstName + ' ' + lastName + '\n' + 'Age: ' + age + '\n' + street + '\n' + city + ', ' + state + ' ' + zipCode + '\n'); 
+
+
+
+// end program 
