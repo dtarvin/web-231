0f2e6fd7a4fae4c6f66cc664132a5d4430a62c85
David Tarvin <david.tarvin@physiciansmutual.com>
2018-12-05 08:23:51 -0600

    Add header and header-test files

---
+++ week-2/Tarvin-header-test.js
@@ -0,0 +1,13 @@
+/*
+============================================
+; Title:  header-test.js
+; Author: Professor Krasso 
+; Date:   05 December 2018
+; Modified By: David Tarvin
+; Description: Displays a formatted header
+;===========================================
+*/
+
+const header = require('../Tarvin-header.js');
+
+console.log(header.display("David", "Tarvin", "Week 2"));
