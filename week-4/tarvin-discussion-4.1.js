82cba968fe6d4bfb03027a90a418b55ef16951f9
David Tarvin <david.tarvin@physiciansmutual.com>
2018-12-28 08:00:37 -0600

    add array program with errors discussion 4.1

---
+++ week-4/tarvin-discussion-4.1.js
@@ -0,0 +1,21 @@
+/*
+============================================
+; Title:  tarvin-discussion-4.1.js
+; Author: David Tarvin
+; Date:   28 December 2018
+; Description: Array program with errors
+;===========================================
+*/
+
+const vehicles = ['car', 'plane', 'train', 'bike', 'scooter'];
+const specialVehicles = [];
+for (let i = 0; i < vehicles.length; i++) {
+    if (vehicles['i'].length > 4) {
+        specialVehicles[i] = vehicles[i];
+    } else {
+        specialVehicles[i] = 'nada';
+    }
+    console,log(specialVehicles[i]);
+}
+
+
