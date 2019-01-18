f181b29522e9491e8293cc5406290b7fdd7080cd
David R Tarvin Jr <david.tarvin@gmail.com>
2019-01-18 15:36:59 -0600

    add exercise 6.3

---
+++ week-6/tarvin-exercise-6.3.js
@@ -0,0 +1,21 @@
+/*
+============================================
+; Title:  tarvin-exercise-6.3.js
+; Author: David Tarvin
+; Date:   17 January 2019
+; Description: Object Literals
+;===========================================
+*/
+
+// display header at beginning of program
+const header = require('../Tarvin-header.js');
+console.log(header.display("David", "Tarvin", "Exercise 6.3"));
+console.log("");
+
+var ticket = {
+    id: 10231,
+    name: "Gary Johnson",
+    requestor: "David Niven"
+};
+
+console.log(ticket);
